23d8462a121fd6e7e0a57abcea10d725fbc16b2e
boben195 <boben195@ukr.net>
2025-07-29 10:54:58 +0300

    add footer

---
+++ my-app/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+      <footer>
+          <p className="txt-[#495057] text-sm text-center px-4 md:px-12 py-4 md:py-6">Made with love by ME</p>
+    </footer>
+  )
+}
+
+export default Footer
