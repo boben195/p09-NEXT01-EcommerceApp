a742fda44eb152249b197bdb46f1a4da2d903396
boben195 <boben195@ukr.net>
2025-07-29 21:14:34 +0300

    conn to cloudinary continue..

--- my-app/utils/addAction.ts
+++ my-app/utils/addAction.ts
@@ -1,5 +1,7 @@
 "use server";
-import {connectDB} from "@/app/api/db/connectDB";
+import { connectDB } from "@/app/api/db/connectDB";
+import cloudinary from "@/utils/cloudinary";
+import Product from "@/app/api/models/product.model";
 
 export async function addAction(formData: FormData) {
     try {
@@ -14,9 +16,41 @@ export async function addAction(formData: FormData) {
         }
 
         await connectDB()
-        //image proces
-    } catch (error) {
         
+
+        const arrayBuffer = await image.arrayBuffer();
+        const buffer = new Uint8Array(arrayBuffer);
+        const imageResponse: any = await new Promise((resolve, reject) => {
+        cloudinary.uploader
+        .upload_stream(
+          {
+            resource_type: "auto",
+            folder: "watch",
+          },
+          async (error, result) => {
+            if (error) {
+              return reject(error.message);
+            }
+            return resolve(result);
+          }
+        )
+        .end(buffer);
+    });
+
+        console.log("Image response: ", imageResponse);
+        await Product.create({
+            image: imageResponse.secure_url,
+            name,
+            price: parseFloat(price as string),/////??? its a string
+            link,
+            description,
+        });
+        return { success: true, message: "Product added successfully" };
+    } catch (error) {
+        return {
+            success: false,
+            message: error instanceof Error ? error.message : "An unexpected error occurred",
+        }
     }
 }
     
