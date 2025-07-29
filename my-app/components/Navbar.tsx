868e8213930d6c01b763c3ddc22474faa58333b3
boben195 <boben195@ukr.net>
2025-07-29 08:50:16 +0300

    add base + navbar

---
+++ my-app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+        <nav>
+          <div>
+              <Link href={"/"}>Zwatches</Link>
+            </div>
+        </nav>
+      )
+}
+
+export default Navbar
