5265fa423d4145588f830cfb49bfefd095c7e140
boben195 <boben195@ukr.net>
2025-07-30 17:39:06 +0300

    add search to navbar

--- my-app/components/Navbar.tsx
+++ my-app/components/Navbar.tsx
@@ -1,7 +1,20 @@
+"use client";
+
 import { Search } from "lucide-react";
 import Link from "next/link";
+import { ChangeEvent } from "react";
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
+  const router = useRouter();
+  const handleChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", e.target.value);
+    const searchQuery = urlParams.toString();
+    router.push(`/search?${searchQuery}`);
+
+
+  }
   return (
         <nav className="px-4 md:px-12 py-4 md:py-6 bg-white text-black">
           <div className="flex items-center justify-between">
@@ -10,7 +23,7 @@ const Navbar = () => {
                   <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                       <Search className="w-4 h-4"/>
                   </div>
-                  <input type="text" className="h-[36px] relative pl-10 border-[1px] border-black/[0.7] text-sm rounded-[8px] w-full py-2 px-3 focus:outline-none bg-transparent" placeholder="Search" />
+                  <input type="text" onChange={handleChange} className="h-[36px] relative pl-10 border-[1px] border-black/[0.7] text-sm rounded-[8px] w-full py-2 px-3 focus:outline-none bg-transparent" placeholder="Search" />
               </div>
               <Link href={"/add-product"}>
                   <button className="bg-[#212529] text-white px-3 py-2 rounded-md cursor-pointer">Add Product</button>
