3e0d5a7f021eef78e64c0f44df56f2b4f5b19971
boben195 <boben195@ukr.net>
2025-07-30 17:27:09 +0300

    some mistakes in style

--- my-app/components/ProductList.tsx
+++ my-app/components/ProductList.tsx
@@ -1,23 +1,37 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect, useState } from 'react'
 import Link from "next/link";
 import Image from 'next/image';
+import axios from 'axios';
+
+
+interface Product {
+    _id: string;
+    image: string;
+    name: string;
+    price: number;
+    }
 
 const ProductList = () => {
-    const products = [
-        { id: 1, name: 'Product 1', price: '$10' },
-        { id: 2, name: 'Product 2', price: '$20' },
-        { id: 3, name: 'Product 3', price: '$30' },
-    ];
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get("api/fetch-products")
+        .then((responce) => setProducts(responce.data.products));
+        
+}, [])
+
   return (
       <div id="product" className="px-4 md:px-12 py-5 md:py-10 flex justify-center items-center">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-5">
-              {products.map((product, index) => (
-                  <Link href="/product/123" key={index} >
-                      <Image src="/dummy-img.jpg" alt="your choise" width={1000} height={1000} className="max-w-[17rem]
-                       h-72 object-center rounded-lg"/>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+              {products.map((product: Product, index) => (
+                  <Link href={`/product/${product._id}`} key={index} >
+                      <Image src={product.image} alt="your choise" width={1000} height={1000} className="max-w-[17rem]
+                       h-72 object-center object-cover rounded-lg"/>
                       <div>
-                          <h2 className="font-semibold text-lg text-black">A very good watch</h2>
-                          <p className="font-medium text-sm mt-1 text-gray-700">$1200</p>
+                          <h2 className="font-semibold text-lg text-black">{ product.name}</h2>
+                          <p className="font-medium text-sm mt-1 text-gray-700">{ product.price}</p>
                       </div>
                   </Link>
               ))}
